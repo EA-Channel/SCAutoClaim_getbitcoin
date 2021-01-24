29a23f00973e4e65feffde7aaff40c9427820ad4
EA-Channel <49808486+EA-Channel@users.noreply.github.com>
2021-01-24 15:12:52 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+//BAHAN YANG PERLU DIINSTAL DI TERMUX
+//[1]. pkg install php
+//[2]. pkg install tesseract
+//[3]. pkg install imagemagick
+//Sampai Sini sudah Paham Kan 
+//Jangan Lupa Subscribe Chanel Penghasil Gratisa
+
+
+$ua = "Mozilla/5.0 (Linux; Android 10; SM-A310F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.185 Mobile Safari/537.36";
+
+$PHPSESSID = "PHPSESSID=j7pk558hf5e0ve5j55kskcusf6";
+
+$get_id = "70944";
